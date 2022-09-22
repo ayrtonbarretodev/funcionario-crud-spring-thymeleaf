54ca9c1d5ac84e0ddb18e1567e85551d60d4ebf2
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-22 14:54:52 -0300

    commit inicial do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# funcionario-crud-spring-thymeleaf
---
+++ src/main/java/com/br/ayrton/funcionariothymeleafcrud/FuncionarioThymeleafCrudApplication.java
@@ -0,0 +1,13 @@
+package com.br.ayrton.funcionariothymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioThymeleafCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionarioThymeleafCrudApplication.class, args);
+    }
+
+}
